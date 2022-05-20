1f5b40e3e0cff79b2a02c4892922928f9cf4ae57
costamay <titusouko@gmail.com>
2022-05-20 19:38:49 +0300

    Completed assignement

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(student_input)
+        @knowledge << student_input
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
